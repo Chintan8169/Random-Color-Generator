76d2d0e65a08a8693461ba39a8b0d2864e182d8a
Chintanrajsinh Gohil <gohilchintanrajsinh8169@gmail.com>
2021-03-22 15:29:32 +0530

    Adding copy to clipboard feature

--- static/index.js
+++ static/index.js
@@ -1,16 +1,42 @@
 const colorContainer = document.querySelector(".colorContainer");
+const msg = document.querySelector(".msg");
 for (let i = 1; i <= 200; i++) {
+	let colorDiv = document.createElement("div");
 	let color = document.createElement("div");
 	color.classList.add("color");
-	colorContainer.appendChild(color);
+	colorDiv.classList.add("colorDiv");
+	const i = document.createElement("i");
+	i.classList.add("fas");
+	i.classList.add("fa-clipboard");
+	i.classList.add("clipBoard");
+	colorContainer.appendChild(colorDiv);
+	colorDiv.appendChild(color);
+	colorDiv.appendChild(i);
 }
+
+let color = document.querySelectorAll(".clipBoard");
+color.forEach(e => {
+	e.addEventListener("click", () => {
+		navigator.clipboard.writeText(e.value).then(() => {
+			msg.classList.add("display");
+			setTimeout(() => {
+				msg.classList.remove("display");
+			}, 2000);
+		}, err => {
+			console.error('Async: Could not copy text: ', err);
+		});
+	});
+})
 const generate = () => {
 	let color = document.querySelectorAll(".color");
-	color.forEach(element => {
-		randomColor = generateColor();
-		element.innerHTML = randomColor;
-		element.style.backgroundColor = randomColor;
-	});
+	let clipBoard = document.querySelectorAll(".clipBoard");
+	for (let i = 0; i < color.length; i++) {
+		let randomColor = generateColor();
+		color[i].innerText = randomColor;
+		color[i].style.backgroundColor = randomColor;
+		clipBoard[i].value = randomColor;
+	};
+
 }
 
 const generateColor = () => {
